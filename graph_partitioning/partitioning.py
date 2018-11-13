cf490a62e2413878fe544b98fc65250628bbba4c
Kasperi Palkama <kasperi.palkama@gmail.com>
2018-11-13 19:44:47 +0200

    small style stuff

--- graph_partitioning/partitioning.py
+++ graph_partitioning/partitioning.py
@@ -13,21 +13,21 @@ class Partition:
         self.k_partition = k
 
     def execute(self, filename=None):
-        adj_mat_laplacian = self.__laplacian()
-        eigenvectors = self.__eigevectors(adj_mat_laplacian)
+        adj_mat_laplacian = self._laplacian()
+        eigenvectors = self._eigevectors(adj_mat_laplacian)
         norm_eigenvecs = self.normalize(eigenvectors)
 
-        self._results = list(zip(self._graph.nodes(), self.__cluster(norm_eigenvecs)))
+        self._results = list(zip(self._graph.nodes(), self._cluster(norm_eigenvecs)))
 
         if not filename:
             return self._results
 
-        self.to_txt(filename)
+        self._to_txt(filename)
 
-    def __laplacian(self):
+    def _laplacian(self):
         return nx.linalg.normalized_laplacian_matrix(self._graph)
 
-    def __eigevectors(self, X):
+    def _eigevectors(self, X):
         _, vectors = eigsh(X, k=self.k_partition, which="LM")
         return vectors
 
@@ -36,11 +36,11 @@ class Partition:
         l1norm = np.abs(X).sum(axis=1).reshape(X.shape[0], 1)
         return X / l1norm
 
-    def __cluster(self, X):
+    def _cluster(self, X):
         kmeans = KMeans(n_clusters=self.k_partition, init="k-means++", n_jobs=-1).fit(X)
         return kmeans.labels_
 
-    def to_txt(self, filename):
+    def _to_txt(self, filename):
         current_dir = os.path.dirname(os.path.abspath(__file__))
         path = os.path.abspath(os.path.join(current_dir, "results", filename))
         with open(path, "w") as f:
