7bd3642c1d3a2680d0cb2ad7be88bf48d4dfb395
Kasperi Palkama <kasperi.palkama@gmail.com>
2018-12-05 19:07:14 +0200

    lots of stuff

--- graph_partitioning/code/partitioning.py
+++ graph_partitioning/code/partitioning.py
@@ -1,34 +1,49 @@
+from scipy.sparse.linalg import eigsh
+from sklearn.cluster import KMeans
+from networkx.algorithms.cuts import cut_size
+from tqdm import tqdm
+
 import pandas as pd
 import numpy as np
 import networkx as nx
 import os
-from scipy.sparse.linalg import eigsh
-from sklearn.cluster import KMeans
-from tqdm import tqdm
+import argparse
 
+class SpectralClustering:
 
-class Partition:
-
-    def __init__(self, df, k):
+    def __init__(self, df, n_clusters):
         self._graph = nx.from_pandas_edgelist(df)
         self._number_of_nodes = nx.number_of_nodes(self._graph)
         self._number_of_edges = nx.number_of_edges(self._graph)
-        self.k_partition = k
+        self.k_partition = n_clusters
+        self._results = None
+        self.conductance = None
+        self._results = None
 
-    def execute(self, filename=None):
-        adj_mat_laplacian = self._laplacian()
-        eigenvectors = self._eigevectors(adj_mat_laplacian)
-        norm_eigenvecs = self.normalize(eigenvectors)
+    def fit(self, output_path=None, normalized=True, return_cost=False):
 
-        self._results = list(zip(self._graph.nodes(), self._cluster(norm_eigenvecs)))
+        if normalized:
+            adj_mat_laplacian = self._normalized_laplacian()
+            k_eigenvectors = self.normalize(self._eigevectors(adj_mat_laplacian))
+        else:
+            adj_mat_laplacian = self._laplacian()
+            k_eigenvectors = self._eigevectors(adj_mat_laplacian)
 
-        if not filename:
-            return self._results
+        self._results = list(zip(self._graph.nodes(), self._cluster(k_eigenvectors)))
+        if not output_path:
+            if return_cost:
+                self.compute_cost()
+                return self._results, self.conductance
+            else:
+                return self._results
 
-        self._to_txt(filename)
+        self._to_txt(output_path)
+
+    def _normalized_laplacian(self):
+        return nx.linalg.normalized_laplacian_matrix(self._graph)
 
     def _laplacian(self):
-        return nx.linalg.normalized_laplacian_matrix(self._graph)
+        return nx.linalg.laplacian_matrix(self._graph).asfptype()
 
     def _eigevectors(self, X):
         _, vectors = eigsh(X, k=self.k_partition, which="LM")
@@ -43,33 +58,56 @@ class Partition:
         kmeans = KMeans(n_clusters=self.k_partition, init="k-means++", n_jobs=-1).fit(X)
         return kmeans.labels_
 
-    def _to_txt(self, filename):
-        current_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        path = os.path.abspath(os.path.join(current_dir, "results", filename))
-        with open(path, "w") as f:
+    def _to_txt(self, output_path):
+
+        with open(output_path, "w") as f:
             f.write("# {0} {1} {2} {3}\n".format(filename.split(".")[0],
                                                  self._number_of_nodes,
                                                  self._number_of_edges,
                                                  self.k_partition))
             f.write('\n'.join('%s %s' % x for x in self._results))
 
+    def compute_cost(self):
+        # conductance of the final partitions
+        arr = np.array(self._results)
+        cut_size_sum = 0
+
+        smallest_partition_size = self._number_of_nodes
+        for i in np.unique(arr[:, 1]):
+            nodes_in_partition = [node_id for node_id, partition_id in self._results if partition_id == i]
+            cut_size_sum += cut_size(self._graph, nodes_in_partition)
+
+            partition_size = len(nodes_in_partition)
+            if partition_size < smallest_partition_size:
+                smallest_partition_size = partition_size
+
+        self.conductance = cut_size_sum / smallest_partition_size
+
+
 
 if __name__ == "__main__":
-    filenames = ["ca-GrQc.txt", "Oregon-1.txt", "soc-Epinions1.txt", "web-NotreDame.txt", "roadNet-CA.txt"]
+
+    parser = argparse.ArgumentParser(description='Graph partitioning')
+    parser.add_argument("--k", help="the number of partitions")
+    args = parser.parse_args()
+
     root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    dataset_dir = os.path.join(root_dir, "data")
+    result_dir = os.path.abspath(os.path.join(root_dir, "results"))
 
-    for filename in tqdm(filenames):
-        graph_filepath = os.path.join(root_dir, "data", "graphs", filename)
-        output_filename = filename + ".output.txt"
+    datasets = [f for f in os.listdir(dataset_dir) if f.endswith(".txt")]
+    for filename in tqdm(datasets):
+        dataset_filepath = os.path.join(dataset_dir, filename)
+        output_filepath = os.path.join(result_dir, filename.strip(".txt") + ".output.txt")
 
-        with open(graph_filepath) as f:
-            first_line = f.readline()
-            k = int(first_line.split(" ")[-1])
 
-        graph_data = pd.read_csv(filepath_or_buffer=graph_filepath,
+
+        graph_data = pd.read_csv(filepath_or_buffer=dataset_filepath,
                                  delimiter=" ",
                                  skiprows=[0],
                                  header=None,
                                  names=["source", "target"])
 
-        p = Partition(graph_data, k).execute(filename=output_filename)
+        SpectralClustering(graph_data, int(args.k)).fit(output_path=output_filepath, normalized=True)
+
+
